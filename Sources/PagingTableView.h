a83e7f5330d34b08c5710fe132037d59d91b89f6
InJung Chung <mu29@yeoubi.net>
2017-04-16 11:58:38 +0900

    Initial commit

---
+++ Sources/PagingTableView.h
@@ -0,0 +1,19 @@
+//
+//  PagingTableView.h
+//  PagingTableView
+//
+//  Created by InJung Chung on 2017. 4. 16..
+//  Copyright © 2017 InJung Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PagingTableView.
+FOUNDATION_EXPORT double PagingTableViewVersionNumber;
+
+//! Project version string for PagingTableView.
+FOUNDATION_EXPORT const unsigned char PagingTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagingTableView/PublicHeader.h>
+
+
---
+++ Tests/PagingTableViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  PagingTableViewTests.swift
+//  PagingTableViewTests
+//
+//  Created by InJung Chung on 2017. 4. 16..
+//  Copyright © 2017 InJung Chung. All rights reserved.
+//
+
+import XCTest
+@testable import PagingTableView
+
+class PagingTableViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
